3b920ebc43920d0f2d9a973e90ad94307777a20b
Janloong-Doo <807110586@qq.com>
2021-01-07 18:09:08 +0800

    update

---
+++ src/views/study/Study.vue
@@ -0,0 +1,26 @@
+<template>
+    <span>sssss</span>
+    <button @click="f">获取用户信息</button>
+</template>
+
+<script lang="ts">
+// import defAxios from '/@/utils/http/index.ts'
+import {getUserInfoById} from "/@/api/User";
+
+export default {
+    name: "Study",
+    setup() {
+        const f = () => {
+            let userInfoById = getUserInfoById({id: "1"});
+            console.log("userInfoById", userInfoById)
+        }
+        return {
+            f
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
